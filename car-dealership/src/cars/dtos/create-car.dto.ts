f0c49ded472aa8311db6a47038959427e24a0f1b
Joseph Morales <josephmss2799@gmail.com>
2024-09-18 12:24:57 -0600

    feat: use validation pipes

--- car-dealership/src/cars/dtos/create-car.dto.ts
+++ car-dealership/src/cars/dtos/create-car.dto.ts
@@ -1,4 +1,8 @@
+import { IsString } from 'class-validator';
+
 export class CreateCardDTO {
+  @IsString()
   readonly brand: string;
+  @IsString()
   readonly model: string;
 }
--- car-dealership/src/main.ts
+++ car-dealership/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove all properties if not exist in the dto definition
+      forbidNonWhitelisted: true, //throw error if req has not valid properties
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
